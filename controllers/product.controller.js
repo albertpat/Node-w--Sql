d518143f032d2b2517718411e17063e537cf1ae5
Albert Pattarachote <albertpattarachote22@gmail.com>
2024-02-20 05:28:38 -1000

    updated

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -11,7 +11,7 @@ function findAll() {
 }
 
 async function findOne(id) {
-    return await query('SELECT * FROM products WHERE id = ?', [id]);
+    return await query('SELECT * FROM products WHERE ProductID = ?', [id]);
 }
 
 async function addOne(name, price, description) {
@@ -19,11 +19,11 @@ async function addOne(name, price, description) {
 }
 
 async function updateOne(name, price, description, id) {
-    return await query('UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?', [name, price, description, id]);
+    return await query('UPDATE products SET name = ?, price = ?, description = ? WHERE ProductID = ?', [name, price, description, id]);
 }
 
 async function removeOne(id) {
-    return await query('DELETE FROM products WHERE id = ?', [id]);
+    return await query('DELETE FROM products WHERE ProductID = ?', [id]);
 }
 
 export {
